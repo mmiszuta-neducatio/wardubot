7fa6973a6114aed70b6aae9ebba067c29a1999b7
mmiszuta-neducatio <mmiszuta@n-educatio.pl>
2017-03-10 16:29:19 +0100

    app ready, needs refactoring

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 'use strict';
 
+var schedule  = require('node-schedule');
 var data      = require('./lib/data-download.js');
 var wardubot  = require('./lib/wardubot.js');
 
+var launchingMessagesSchedule = new schedule.RecurrenceRule();
+launchingMessagesSchedule.dayOfWeek = [1,2,3,4,5,6,7];
+launchingMessagesSchedule.hour = 10;
+launchingMessagesSchedule.minute = 20;
 
+var dataUpdateSchedule = new schedule.RecurrenceRule();
+dataUpdateSchedule.dayOfWeek = [1,4];
+dataUpdateSchedule.hour = 8;
+dataUpdateSchedule.minute = 20;
+
+schedule.scheduleJob(dataUpdateSchedule, function(){
 data.updateForSlack();
+});
 
-setTimeout(function(){
+
+ schedule.scheduleJob(launchingMessagesSchedule, function(){
   wardubot.sendMessages();
-}, 50000)
+});
