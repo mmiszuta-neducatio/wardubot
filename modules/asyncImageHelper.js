9564ec043bff20852c04d87d3bb7e57c8d08e904
mmiszuta-neducatio <mmiszuta@n-educatio.pl>
2017-03-14 14:53:06 +0100

    code beautified

--- modules/asyncImageHelper.js
+++ modules/asyncImageHelper.js
@@ -1,37 +1,41 @@
-/*jslint node: true */
-"use strict";
+'use strict';
 
-var async = require("async");
-var imageHelper = require("../lib/imageHelper.js");
-var fs = require("fs");
-
-
+var async       = require('async');
+var imageHelper = require('../lib/imageHelper.js');
+var fs          = require('fs');
 
 module.exports = {
-  downloadAll: function(productsImageLinks, cb){
+  downloadAll: function (productsImageLinks, cb) {
     var counter = 0;
-    async.eachSeries(productsImageLinks, function(link, callback){
-      var pathToRawImage = "./images/raw/rawImage" + counter++ + ".jpg";
-      imageHelper.downloadImage({url: link, headers: {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0', 'Referer': 'http://www.biedronka.pl/pl', 'Content-Type': 'image/jpeg'}}, pathToRawImage, callback);
+    async.eachSeries(productsImageLinks, function (link, callback) {
+      var pathToRawImage = './images/raw/rawImage' + counter++ + '.jpg';
+      imageHelper.downloadImage({
+        url: link,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0',
+          'Referer': 'http://www.biedronka.pl/pl',
+          'Content-Type': 'image/jpeg'
+        }
+      }, pathToRawImage, callback);
     }, cb);
   },
-  editAll: function(productsPricesTotal, cb){
+  editAll: function (productsPricesTotal, cb) {
     var rawImages = fs.readdirSync('./images/raw/');
-    var counter = 0;
-    async.eachSeries(rawImages, function(rawImageName, callback){
+    var counter   = 0;
+    async.eachSeries(rawImages, function (rawImageName, callback) {
       console.log('.');
       imageHelper.editImages(counter++, productsPricesTotal, callback);
     }, cb);
   },
-  addProductDataToArray: function(arrayForProducts, counter, urlsForSlack, productsLinks, callback){
-    function ProductDataForSlack(id, cloudinaryUrl, productLink)
-    {
-      this.id = id;
-      this.imgUrl = cloudinaryUrl;
-      this.productLink = productLink;
+  addProductDataToArray: function (arrayForProducts, counter, urlsForSlack, productsLinks, callback) {
+    function ProductDataForSlack(id, cloudinaryUrl, productLink) {
+      this.id           = id;
+      this.imgUrl       = cloudinaryUrl;
+      this.productLink  = productLink;
     }
     arrayForProducts.push(new ProductDataForSlack(counter, urlsForSlack, productsLinks[counter]));
-    fs.writeFileSync('./data.json', JSON.stringify(arrayForProducts, null,2),'utf-8');
+    fs.writeFileSync('./data.json', JSON.stringify(arrayForProducts, null, 2), 'utf-8');
+
     callback(null, arrayForProducts);
   }
 };
--- modules/asyncRequestHelper.js
+++ modules/asyncRequestHelper.js
@@ -1,82 +1,89 @@
-/*jslint node: true */
-"use strict";
+'use strict';
 
-var async             = require("async");
-var tinyreq           = require("tinyreq");
-var cheerio           = require("cheerio");
-var scrapeHelper      = require("../lib/scrapeHelper.js");
-var imageHelper       = require("../lib/imageHelper.js");
-var asyncImageHelper  = require("./asyncImageHelper.js");
-var uploader          = require("../lib/uploader.js");
+var async             = require('async');
+var tinyreq           = require('tinyreq');
+var cheerio           = require('cheerio');
+var scrapeHelper      = require('../lib/scrapeHelper.js');
+var imageHelper       = require('../lib/imageHelper.js');
+var asyncImageHelper  = require('./asyncImageHelper.js');
+var uploader          = require('../lib/uploader.js');
 
 
 
 
 module.exports = {
-  preparationRequest: function(callback){
-    var baseUrl = "http://www.biedronka.pl";
-    tinyreq(baseUrl, function(err, body){
+  preparationRequest: function (callback) {
+    var baseUrl = 'http://www.biedronka.pl';
+    tinyreq(baseUrl, function (err, body) {
+      if (err) {
+        throw err;
+      }
+      var i                         = 0;
+      var completePromotionsLinks   = [];
+      var $                         = cheerio.load(body);
+      var incompletePromotionsLinks = scrapeHelper.getAttribute($, $('a[title=\'Akcje Tematyczne\']').next().find('a'), 'href');
+      var promotionsTexts           = scrapeHelper.getText($, $('a[title=\'Akcje Tematyczne\']').next().find('a'));
+      var regex                     = new RegExp('.*[0-9]{2}\.[0-9]{2}');
 
-      var completePromotionsLinks = [];
-      if(err) {throw err;}
-      var $ = cheerio.load(body);
-      var incompletePromotionsLinks = scrapeHelper.getAttribute($, $("a[title=\"Akcje Tematyczne\"]").next().find("a"), "href");
-      var promotionsTexts = scrapeHelper.getText($, $("a[title=\"Akcje Tematyczne\"]").next().find("a"));
-      var regex = new RegExp(".*[0-9]{2}\.[0-9]{2}");
-      for(var i = 0; i < incompletePromotionsLinks.length; i++) {
-        if(regex.test(promotionsTexts[i])){
+      for (i = 0; i < incompletePromotionsLinks.length; i++) {
+        if (regex.test(promotionsTexts[i])) {
           completePromotionsLinks.push(baseUrl + incompletePromotionsLinks[i]);
         }
       }
       callback(null, completePromotionsLinks);
     });
   },
-  mainRequest: function(link, callback){
-    var baseUrl = "http://www.biedronka.pl";
-    tinyreq(link, function(err, body){
-      if(err) {throw err;}
-      var $ = cheerio.load(body);
+  mainRequest: function (link, callback) {
+    var i       = 0;
+    var baseUrl = 'http://www.biedronka.pl';
+    tinyreq(link, function (err, body) {
+      if (err) {
+        throw err;
+      }
+      var $                       = cheerio.load(body);
       var completeProductsHrefs   = [];
       var productsPricesTotal     = [];
-      var productsImageLinks      = scrapeHelper.getAttribute($, $(".productsimple-default img"), "src");
-      var incompleteProductsHrefs = scrapeHelper.getAttribute($, $(".productsimple-default a"), "href");
-      var productsPricesPln       = scrapeHelper.getText($, $(".pln"));
-      var productsPricesGr        = scrapeHelper.getText($, $(".gr"));
+      var productsImageLinks      = scrapeHelper.getAttribute($, $('.productsimple-default img'), 'src');
+      var incompleteProductsHrefs = scrapeHelper.getAttribute($, $('.productsimple-default a'), 'href');
+      var productsPricesPln       = scrapeHelper.getText($, $('.pln'));
+      var productsPricesGr        = scrapeHelper.getText($, $('.gr'));
 
-      for(var i = 0; i < incompleteProductsHrefs.length; i++) {
+      for (i = 0; i < incompleteProductsHrefs.length; i++) {
         completeProductsHrefs.push(baseUrl + incompleteProductsHrefs[i]);
       }
 
-      for (i=0; i < productsPricesPln.length; i++){
-        productsPricesTotal.push(productsPricesPln[i] + "," + productsPricesGr[i] + " pln");
+      for (i = 0; i < productsPricesPln.length; i++) {
+        productsPricesTotal.push(productsPricesPln[i] + ',' + productsPricesGr[i] + ' pln');
       }
 
       async.series([
-        function(next){
-          console.log("Downloading images, please wait");
-          asyncImageHelper.downloadAll(productsImageLinks, next);
-        },
-        function(next){
-          console.log("creating price bars");
-          imageHelper.createRectangle(next);
-        },
-        function(next){
-          console.log("creating images with price bars");
-          asyncImageHelper.editAll(productsPricesTotal, next);
-        },
-        function(next){
-          uploader.cloudUpload(next);
-        }],
-        function(imageUrlsForSlack){
+          function (next) {
+            console.log('Downloading images, please wait');
+            asyncImageHelper.downloadAll(productsImageLinks, next);
+          },
+          function (next) {
+            console.log('creating price bars');
+            imageHelper.createRectangle(next);
+          },
+          function (next) {
+            console.log('creating images with price bars');
+            asyncImageHelper.editAll(productsPricesTotal, next);
+          },
+          function (next) {
+            uploader.cloudUpload(next);
+          }
+        ],
+        function (imageUrlsForSlack) {
           var productsForSlack = [];
           var counter = 0;
-          async.eachSeries(imageUrlsForSlack, function(imageUrl, cb){
-            asyncImageHelper.addProductDataToArray(productsForSlack, counter++, imageUrl, completeProductsHrefs, cb);
-          }, function(){
-            console.log("done");
-            callback();
-          });
+          async.eachSeries(imageUrlsForSlack, function (imageUrl, cb) {
+              asyncImageHelper.addProductDataToArray(productsForSlack, counter++, imageUrl, completeProductsHrefs, cb);
+            },
+            function () {
+              console.log('done');
+              callback();
+            });
         });
-      });
-    }
-  };
+    });
+  }
+};
