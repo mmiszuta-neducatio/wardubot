ff50e3c1c61453841c3f51aa94e0720135e62849
mmiszuta-neducatio <mmiszuta@n-educatio.pl>
2017-03-22 10:25:33 +0100

    fixed a bug where it couldnt be started properly on particular days

--- app.js
+++ app.js
@@ -11,16 +11,17 @@ dataUpdateSchedule.minute     = 20;
 
 var launchingMessagesSchedule       = new schedule.RecurrenceRule();
 launchingMessagesSchedule.dayOfWeek = [1,2,3,4,5,6,7];
-launchingMessagesSchedule.hour      = 10;
-launchingMessagesSchedule.minute    = 20;
+launchingMessagesSchedule.hour      = 11;
+launchingMessagesSchedule.minute    = 12;
+
+//when it starts it needs to get images to start working.
+data.updateForSlack();
 
 console.log('application initialized. Scheduled jobs will start at specific time. Please do not close the app!');
 schedule.scheduleJob(dataUpdateSchedule, function () {
   data.updateForSlack();
 });
 
-
-
-//schedule.scheduleJob(launchingMessagesSchedule, function () {
+schedule.scheduleJob(launchingMessagesSchedule, function () {
   wardubot.sendMessages();
-//});
+});
